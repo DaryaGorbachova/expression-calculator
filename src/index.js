c82a9a8dc9b0c2d5145881430aa38f88f445b377
DaryaGorbachova <vrindavaneshvari@mail.ru>
2021-08-28 17:54:11 +0300

    feat: can recognize a few digit numbers

--- src/index.js
+++ src/index.js
@@ -4,8 +4,51 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+	let array = [];
+	let result = 0;
+	let char = '';	
+	expr = expr + ' ';
+	for (let i = 0; i < expr.length; i++) {
+
+		if(expr[i] === '+'
+		  || expr[i] === '-'
+		  || expr[i] === '/'
+		  || expr[i] === '*') array.push(expr[i])
+		else if (isNaN(parseInt(expr[i])) === false) {
+			char += expr[i];
+		} else if (expr[i] === ' ' && char !== '') {
+			char = parseInt(char);
+			array.push(char);	
+			char = '';
+		}
+		
+	}
+
+result = array[0];
+	
+for (let i = 0; i < array.length - 1; i++) {
+	if (array[i] === '*') {
+
+		result *= array[i + 1];
+	}
+	else if (array[i] === '/') {
+
+		result /= array[i + 1];
 }
+}
+for (let i = 0; i < array.length - 1; i++) {
+	if (array[i] === '+') result += array[i + 1];
+	else if (array[i] === '-') result -= array[i + 1];
+}
+	
+	return result;
+}
+
+expressionCalculator('10 + 9');
+
+
+expressionCalculator(9*10)
+
 
 module.exports = {
     expressionCalculator
